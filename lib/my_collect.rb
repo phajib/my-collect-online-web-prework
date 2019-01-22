e753167b49a8dcd89e910b499031e7eecbfd442e
H B <phajib@gmail.com>
2019-01-22 00:06:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,15 @@
 def my_collect(array)
   new_array = []
+  i = 0
+
+  while i < array.length
+    new_array.push(yield[i])
+    i += 1 
+=begin
   array.each do |str|
     str.split(" ").first
     new_array << str.upcase
   end
   array
+=end  
 end
