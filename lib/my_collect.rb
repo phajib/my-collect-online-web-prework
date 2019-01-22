6fc4a01439d32cf05fd888e8e959f3ff87b8c501
H B <phajib@gmail.com>
2019-01-22 00:27:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,9 +4,6 @@ def my_collect(array)
   while i < array.length
     new_array.push yield (array[i])
     i += 1
-    #new_array.push(yield[i]) do |str|
-    #  str.split(" ").first
-    #end
   end
   new_array
 end
