e501e4220fc8c94b3be5da4242d1960124a09898
H B <phajib@gmail.com>
2019-01-22 00:09:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,7 +6,7 @@ def my_collect(array)
     new_array.push(yield[i])
     i += 1
   end
-  new_array 
+  new_array
 =begin
   array.each do |str|
     str.split(" ").first
@@ -14,4 +14,8 @@ def my_collect(array)
   end
   array
 =end
+end
+
+my_collect(students) do |name|
+  name.split(" ").first
 end
