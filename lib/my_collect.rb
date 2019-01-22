79b5bb6130c7f80e2b1cb48a698ae7be22d1ac74
H B <phajib@gmail.com>
2019-01-22 00:03:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,8 @@
 def my_collect(array)
   new_array = []
-  array.each do |word|
-    new_array << word.upcase
+  array.each do |str|
+    str.split(" ").first
+    #new_array << str.upcase
   end
   array
 end
